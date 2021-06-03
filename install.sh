f11e421faba00e757b321e20dc5be8b075d40a9f
kdehal <kdehal@gmail.com>
2021-06-03 09:24:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ cd impact-analysis
 sudo docker secret create database-credentials.txt database-credentials.txt
 sudo docker secret create windchill-credentials.txt windchill-credentials.txt
 #update ip address, replace 999.999.9.99 with the IP address of the server
-sed -i "s/192.168.1.14/$1/g" configurations/systemConfig.json  
+ip_address=$(hostname -I)
+sed -i "s/192.168.1.14/$ip_address/g" configurations/systemConfig.json  
+sed -i "s/HOSTNAME/$HOSTNAME/g" configurations/systemConfig.json  
 #start stack
 sudo docker stack deploy --compose-file docker-compose-swarm.yml impact
