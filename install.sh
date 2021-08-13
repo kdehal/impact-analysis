b512d93ffda3f888d1fabaadd17685877f076f07
Kar Dehal <kdehal@gmail.com>
2021-08-13 13:53:10 -0700

    fixed missing docker secret for messagebroker

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo docker swarm init
 cd impact-analysis
 sudo docker secret create database-credentials.txt database-credentials.txt
 sudo docker secret create windchill-credentials.txt windchill-credentials.txt
+sudo docker secret create messagebroker-credentials.txt messagebroker-credentials.txt
 #update ip address, replace 999.999.9.99 with the IP address of the server
 ip_address=$(hostname -I)
 sed -i "s/192.168.1.14/$ip_address/g" configurations/systemConfig.json  
